15ef700b01aac956649522236d4e3bd91c13b319
DESKTOP-5V3FRMH\M <gidal569@gmail.com>
2025-12-09 18:50:17 +0900

    1. 손전등 토글, 배터리 구현

---
+++ Assets/FaintFear/_Script/Flashlight.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light spotLight;              // 손전등
+    private bool isOn = false;            // 현재 상태
+
+    [SerializeField]
+    private float maxBattery = 100f;       // 최대 배터리  
+    [SerializeField]
+    private float currentBattery;          // 현재 배터리  
+    [SerializeField]
+    private float batteryDrainRate = 10f; // 1초에 감소할 배터리량
+
+    private PlayerInputAction inputActions;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputAction();
+
+        // Flashlight Action이 눌렸을 때
+        inputActions.Player.Interaction.performed += ctx => ToggleLight();
+    }
+
+    private void Start()
+    {
+        //초기화
+        currentBattery = maxBattery;
+        isOn = false;
+        spotLight.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    private void Update()
+    {
+        if (isOn)
+        {
+            DrainBattery();
+        }
+    }
+
+    void ToggleLight()
+    {
+        // 배터리가 없으면 못 켠다
+        if (currentBattery <= 0f)
+        {
+            spotLight.enabled = false;
+            isOn = false;
+            return;
+        }
+
+        //손전등 토글
+        isOn = !isOn;
+        spotLight.enabled = isOn;
+    }
+
+    //손전등 배터리 소모
+    void DrainBattery()
+    {
+        currentBattery -= batteryDrainRate * Time.deltaTime;
+
+        if (currentBattery <= 0f)
+        {
+            currentBattery = 0f;
+            spotLight.enabled = false;
+            isOn = false;
+        }
+    }
+}
