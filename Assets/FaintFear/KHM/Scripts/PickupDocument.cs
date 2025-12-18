2ce82db5ed3bed129ff79ac763e5ef6895655e37
DESKTOP-5V3FRMH\M <gidal569@gmail.com>
2025-12-18 15:00:15 +0900

    1. 메인씬에 문서 아이템 기능 추가

--- Assets/FaintFear/KHM/Scripts/PickupDocument.cs
+++ Assets/FaintFear/KHM/Scripts/PickupDocument.cs
@@ -4,7 +4,7 @@ namespace FaintFear
     /// <summary>
     /// 문서 아이템을 줍기
     /// </summary>
-    public class PickupDocument : Interactive
+    public class PickupDocument : Interactive, IActionProvider
     {
         #region Variables
         private PlayerMove playerMove;
@@ -22,11 +22,18 @@ namespace FaintFear
             //플레이어 움직임 막기
             playerMove.enabled = false;
             //문서 UI 보이게
-            DocumentUI.SetActive(true); 
+            DocumentUI.SetActive(true);
             //정신력 시스템 off
             PlayerStatus.Instance.isMentalSystemActive = false;
             //배터리 시스템 off
             PlayerStatus.Instance.isBatteryActive = false;
+        }
+
+        // ActionUI에 표시할 문구 제공
+        public string GetActionText()
+        {
+            // 화면에 표시될 상호작용 문구
+            return "문서";
         }
     }
 }
