bd602d0e6bccaef276f4e8a7859a557a16a31bad
DESKTOP-5V3FRMH\M <julie102604@naver.com>
2025-12-22 17:10:56 +0900

    시야 고정

--- Assets/FaintFear/LEH/Scripts/Camera/PushItemAimController.cs
+++ Assets/FaintFear/LEH/Scripts/Camera/PushItemAimController.cs
@@ -1,16 +1,75 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PushItemAimController : MonoBehaviour
+namespace FaintFear
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PushItemAimController : MonoBehaviour
     {
-        
-    }
+        [Header("Detect")]
+        public float detectDistance = 2.5f;
+        public LayerMask pushItemLayer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Zoom")]
+        public float zoomFOV = 35f;
+        public float zoomSpeed = 8f;
+
+        [Header("Player")]
+        public PlayerMove playerMove;
+
+        Camera mainCam;
+        float defaultFOV;
+        bool isZooming;
+
+        void Awake()
+        {
+            mainCam = Camera.main;
+            defaultFOV = mainCam.fieldOfView;
+        }
+
+        void Update()
+        {
+            bool canPush = DetectPushItem();
+            bool pressingV = Keyboard.current.vKey.isPressed;
+
+            if (canPush && pressingV)
+                StartZoom();
+            else
+                StopZoom();
+
+            UpdateZoom();
+        }
+
+        bool DetectPushItem()
+        {
+            Collider[] hits = Physics.OverlapSphere
+                (transform.position, detectDistance, pushItemLayer);
+
+            return hits.Length > 0;
+        }
+
+        void StartZoom()
+        {
+            if (isZooming) return;
+
+            isZooming = true;
+            playerMove.canMove = false;
+            playerMove.SetLookLock(true);
+        }
+
+        void StopZoom()
+        {
+            if (!isZooming) return;
+
+            isZooming = false;
+            playerMove.canMove = true;
+            playerMove.SetLookLock(false);
+        }
+
+        void UpdateZoom()
+        {
+            float targetFOV = isZooming ? zoomFOV : defaultFOV;
+            mainCam.fieldOfView =
+                Mathf.Lerp(mainCam.fieldOfView, targetFOV, Time.deltaTime * zoomSpeed);
+        }
     }
 }
