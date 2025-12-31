2922ee2d36582b6a0940dacd7003c3b3b1accf12
eoJin1 <dongeojin@gmail.com>
2025-12-31 14:10:56 +0900

    ItemGlowPulse 스크립트 추가

---
+++ Assets/FaintFear/_Script/UI/ItemGlowPulse.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ItemGlowPulse : MonoBehaviour
+{
+    public Color emissionColor = Color.white;
+    public float intensity = 1f;
+    public float pulseSpeed = 1f;
+
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        mat.EnableKeyword("_EMISSION");
+    }
+
+    void Update()
+    {
+        float emission = Mathf.PingPong(Time.time * pulseSpeed, 1f);
+        Color finalColor = emissionColor * (emission * intensity);
+
+        mat.SetColor("_EmissionColor", finalColor);
+    }
+}
